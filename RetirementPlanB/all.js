02f80fb9ebc848ed8d54ec9fa57a28d74aa618ad
Angela Stanchie <astanchi@advent.com>
2015-05-22 12:32:41 -0700

    Retirement Plan B
    Add comments to code, switch winning numbers and jackpot amount

--- RetirementPlanB/all.js
+++ RetirementPlanB/all.js
@@ -1,8 +1,13 @@
+/* draw games*/
+var powerGameId = 1;
+var megaGameId = 2;
+var superGameId = 3;
+var GameId = powerGameId;
 var gameMega = document.querySelector("#gameMega");
 var gameSuper = document.querySelector("#gameSuper");
 var gamePower = document.querySelector("#gamePower");
 var jackpotAmount = $("#nextJackpotAmount");
-
+/* winning Numbers*/
 var ball1Num = $("#ball1 p");
 var ball2Num = $("#ball2 p");
 var ball3Num = $("#ball3 p");
@@ -10,7 +15,7 @@ var ball4Num = $("#ball4 p");
 var ball5Num = $("#ball5 p");
 var megaNum = document.querySelector("#megaBall p");
 var megaBall = document.querySelector("#megaBall");
-
+/* quickpick Numbers */
 var qpBall1Num = $("#qpBall1 p");
 var qpBall2Num = $("#qpBall2 p");
 var qpBall3Num = $("#qpBall3 p");
@@ -18,26 +23,31 @@ var qpBall4Num = $("#qpBall4 p");
 var qpBall5Num = $("#qpBall5 p");
 var qpMegaNum = $("#qpMegaBall p");
 var qpMegaBall = document.querySelector("#qpMegaBall");
-
+/* Draw game number range */
 var pmaxBall = 59;
 var pmaxMega = 35;
 var mmaxBall = 75;
 var mmaxMega = 15;
 var smaxBall = 47;
 var smaxMega = 27;
-
 var minBall = 1;
 var maxBall = 1;
 var minMega = 1;
 var maxMega = 1;
-
-var powerGameId = 1;
-var megaGameId = 2;
-var superGameId = 3;
-
-var GameId = powerGameId;
+/* Draw game colors*/
+pMegaBallBackgroundColor = "#CA1F26"
+pMegaBallBoxShadowColor = "rgba(101, 17, 19,.7)";
+mMegaBallBackgroundColor = "#1A4199"
+mMegaBallBoxShadowColor = "rgba(11, 33, 82,.7)";
+sMegaBallBackgroundColor = "#F48A1E"
+sMegaBallBoxShadowColor = "rgba(201, 80, 14,.7)";
+/* other elements*/
 var qpForm = $("#quickpickForm");
 
+/**********************************************
+Set the draw game minimum and maximum number range for
+regular balls and the Mega ball
+**********************************************/
 function setMinMax (newGameId) {
   if (newGameId == 1) {
     maxBall = pmaxBall;
@@ -51,6 +61,9 @@ function setMinMax (newGameId) {
   }
 }
 
+/**********************************************
+Reset quickpick numbers to default value $
+**********************************************/
 function resetQuickPick (newGameId) {
   if (GameId != newGameId) {
     qpBall1Num.text("$");
@@ -62,6 +75,25 @@ function resetQuickPick (newGameId) {
   }
 }
 
+/**********************************************
+set ball color background and boxshadow
+**********************************************/
+function setBallColor(ball,bkgColor,bxsColor) {
+  ball.style.setProperty("background",bkgColor);
+  ball.style.setProperty("box-shadow","inset -25px -25px 30px " + bxsColor);
+  ball.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px " + bxsColor);
+  ball.style.setProperty("-moz-box-shadow","inset -25px -25px 30px " + bxsColor);
+}
+
+/**********************************************
+Based on the selected draw game
+- set the valid numer ranges
+- reset quickpick generate
+- store the gameId into local storage
+- initialize UI for selected game:
+  - non-selected games as more transparent
+  - set winningNumber and quickpick Mega Ball colors
+**********************************************/
 function setGame (newGameId) {
   setMinMax(newGameId);
   resetQuickPick(newGameId);
@@ -73,75 +105,55 @@ function setGame (newGameId) {
     $("#imgPower").css("opacity",1);
     $("#imgMega").css("opacity",0.4);
     $("#imgSuper").css("opacity",0.4);
+    setBallColor(megaBall,pMegaBallBackgroundColor,pMegaBallBoxShadowColor);
+    setBallColor(qpMegaBall,pMegaBallBackgroundColor,pMegaBallBoxShadowColor);
   } else if (GameId == 2) {
     $("#imgPower").css("opacity",0.4);
     $("#imgMega").css("opacity",1);
     $("#imgSuper").css("opacity",0.4);
+    setBallColor(megaBall,mMegaBallBackgroundColor,mMegaBallBoxShadowColor);
+    setBallColor(qpMegaBall,mMegaBallBackgroundColor,mMegaBallBoxShadowColor);
   } else  {
     $("#imgPower").css("opacity",0.4);
     $("#imgMega").css("opacity",0.4);
     $("#imgSuper").css("opacity",1);
+    setBallColor(megaBall,sMegaBallBackgroundColor,sMegaBallBoxShadowColor);
+    setBallColor(qpMegaBall,sMegaBallBackgroundColor,sMegaBallBoxShadowColor);
   }
 
 }
 
+/**********************************************
+Draw Game: Powerball
+- get data from calottery API
+-   using Avand's proxy server because of error: "Access-Control-Allow-Origin"
+- initialize game attributes
+**********************************************/
 function powerClicked (e) {
-  //$.get("http://calservice.calottery.com/api/drawgames/12",updateLotto);
   $.getJSON("http://proxy.avandamiri.com/get?url=http://calservice.calottery.com/api/drawgames/12",updateLotto);
   setGame(powerGameId);
-  megaBall.style.setProperty("background","#CA1F26");
-  megaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
-  megaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
-  megaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
-  qpMegaBall.style.setProperty("background","#CA1F26");
-  qpMegaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
-  qpMegaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
-  qpMegaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(101, 17, 19,.7)");
 }
 
 function megaClicked (e) {
-  //$.get("http://calservice.calottery.com/api/drawgames/15",updateLotto);
   $.getJSON("http://proxy.avandamiri.com/get?url=http://calservice.calottery.com/api/drawgames/15",updateLotto);
   setGame(megaGameId);
-  megaBall.style.setProperty("background","#1A4199");
-  megaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
-  megaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
-  megaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
-  qpMegaBall.style.setProperty("background","#1A4199");
-  qpMegaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
-  qpMegaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
-  qpMegaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(11, 33, 82,.7)");
 }
 
 function superClicked (e) {
-  //$.get("http://calservice.calottery.com/api/drawgames/8",updateLotto);
   $.getJSON("http://proxy.avandamiri.com/get?url=http://calservice.calottery.com/api/drawgames/8",updateLotto);
   setGame(superGameId);
-  megaBall.style.setProperty("background","#F48A1E");
-  megaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
-  megaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
-  megaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
-  qpMegaBall.style.setProperty("background","#F48A1E");
-  qpMegaBall.style.setProperty("box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
-  qpMegaBall.style.setProperty("-webkit-box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
-  qpMegaBall.style.setProperty("-moz-box-shadow","inset -25px -25px 30px rgba(201, 80, 14,.7)");
 }
 
+/**********************************************
+Format number to have comma as thousandth separator
+**********************************************/
 function formatNumber (num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
 }
 
-function get(url, success) {
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener("readystatechange", function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      success(JSON.parse(xhr.response));
-    }
-  });
-  xhr.open("GET", url,true);
-  xhr.send();
-}
-
+/**********************************************
+Format string to date format mm/dd/ccyy
+**********************************************/
 function getDateFormatted(drawDate) {
   var drawDateYear = drawDate.substring(0,4);
   var drawDateMonth = drawDate.substring(5,7);
@@ -151,6 +163,11 @@ function getDateFormatted(drawDate) {
 
 }
 
+/**********************************************
+When API call is successful, call this function to initialize page with
+- last winning numbers
+- next jackpot amount
+**********************************************/
 function updateLotto(results) {
   var winningNumbers = results.games[0].draws[0].winningNumbers;
   var gamePotAmount = results.games[0].nextJackpot.jackpotAmount;
@@ -176,6 +193,9 @@ function updateLotto(results) {
   $("#quickpickDrawDate").text("Next Draw Date: " + nextDrawDate);
 }
 
+/**********************************************
+Validate user input quick pick numbers are integers
+**********************************************/
 function validateQPNumbersAreIntegers(QPNumbers) {
   var status = 0;
   var notInt = '';
@@ -203,6 +223,9 @@ function validateQPNumbersAreIntegers(QPNumbers) {
   return status;
 }
 
+/**********************************************
+Validate user input quick pick numbers are in the draw game range
+**********************************************/
 function validateQPNumbersAreInRange(QPNumbers) {
   var status = 0;
   var ballNumber = 0;
@@ -243,6 +266,9 @@ function validateQPNumbersAreInRange(QPNumbers) {
   return status;
 }
 
+/**********************************************
+Validate user input quick pick numbers have no duplicates
+**********************************************/
 function validateQPNumbersHasNoDuplicates(QPNumbers) {
   var status = 0;
   var dupMessage = '';
@@ -262,6 +288,9 @@ function validateQPNumbersHasNoDuplicates(QPNumbers) {
   return status;
 }
 
+/**********************************************
+create QP object with attributes
+**********************************************/
 function createQPNumbers(userInput,isMega,userEnteredQPNumbers) {
   var qpBall = {
     qpValue:0,    //user input
@@ -280,13 +309,17 @@ function createQPNumbers(userInput,isMega,userEnteredQPNumbers) {
   return qpBall;
 }
 
-
+/**********************************************
+Returns a random integer between min (included) and max (excluded)
+Using Math.round() will give you a non-uniform distribution!
+**********************************************/
 function getRandomInt(min, max) {
-  // Returns a random integer between min (included) and max (excluded)
-  // Using Math.round() will give you a non-uniform distribution!
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/**********************************************
+sort quickpick numbers in ascending order
+**********************************************/
 function sortQPNumbers(qpNumbers) {
   // sort QP generated numbers
   var sortNumbers= [];
@@ -301,6 +334,9 @@ function sortQPNumbers(qpNumbers) {
   return qpNumbers;
 }
 
+/**********************************************
+generate quickpick numbers
+**********************************************/
 function generateQPNumbers(qpNumbers) {
   var isDuplicate = 0;
   for (var i=0; i < qpNumbers.length; i++) {
@@ -329,6 +365,9 @@ function generateQPNumbers(qpNumbers) {
   return qpNumbers;
 }
 
+/**********************************************
+Set UI quickpick numbers to generated quickpick values
+**********************************************/
 function showQPNumbers(qpNumbers) {
   qpBall1Num.text(qpNumbers[0].qpNumber);
   qpBall2Num.text(qpNumbers[1].qpNumber);
@@ -338,17 +377,21 @@ function showQPNumbers(qpNumbers) {
   qpMegaNum.text(qpNumbers[5].qpNumber);
 }
 
+/**********************************************
+Set UI quickpick numbers to generated quickpick values
+**********************************************/
 function QPFormSubmitted (e) {
   e.preventDefault();
   var qpNumbers = [];
   var userEnteredQPNumbers = [];
-
+  //create an array of QP objects
   qpNumbers.push(createQPNumbers($("#qpInput1").val(),0,userEnteredQPNumbers));
   qpNumbers.push(createQPNumbers($("#qpInput2").val(),0,userEnteredQPNumbers));
   qpNumbers.push(createQPNumbers($("#qpInput3").val(),0,userEnteredQPNumbers));
   qpNumbers.push(createQPNumbers($("#qpInput4").val(),0,userEnteredQPNumbers));
   qpNumbers.push(createQPNumbers($("#qpInput5").val(),0,userEnteredQPNumbers));
   qpNumbers.push(createQPNumbers($("#qpInputMega").val(),1,userEnteredQPNumbers));
+  //validate user input quickpick numbers
   var status = validateQPNumbersAreIntegers(userEnteredQPNumbers);
   if (status == 0) {
     status = validateQPNumbersAreInRange(userEnteredQPNumbers);
@@ -364,14 +407,21 @@ function QPFormSubmitted (e) {
   }
 }
 
+/**********************************************
+event listners for draw games
+**********************************************/
 gameMega.addEventListener("click",megaClicked);
 gameSuper.addEventListener("click",superClicked);
 gamePower.addEventListener("click",powerClicked);
-//qpForm.addEventListener("submit",QPFormSubmitted);
 qpForm.on("submit",QPFormSubmitted);
 
+/**********************************************
+When page is initially loaded,
+  load local storage last selected game
+  or load default game powerball
+**********************************************/
 $( document ).ready(function() {
-  var lastGameId = 1;
+  var lastGameId = 1;//Powerball
   if (localStorage) {
     var lastGameId = parseInt(localStorage.getItem("gameId"));
   }
